85178bd4331663a1fd5dcf02e2c80b1cb5b24b84
David-Senerchia <45632454+David-Senerchia@users.noreply.github.com>
2019-01-25 00:47:51 -0500

    Update script.js

--- en/js/script.js
+++ en/js/script.js
@@ -84,9 +84,9 @@ Vue.component('page-list', {
 Vue.component('banner', {
   template: `<div id='index-header' class='header-strip'>
     <span id="header-info">
-      <a v-for="img in americanFlag" v-bind:href='img.url'><span id='header-name'>{{ <img src='../../img/americanFlag.png' id='flag-img' /> }}</span></a>
-      <a v-for="img in frenchFlag" v-bind:href='img.url'><span id='header-name'>{{ <img src='../../img/frenchFlag.jpg' id='flag-img' /> }}</span></a>
-      <a v-for="img in polishFlag" v-bind:href='img.url'><span id='header-name'>{{ <img src='../../img/polishFlag.jpg' id='flag-img' /> }}</span></a>
+      <a v-for="img in americanFlag" v-bind:href='img.url'><span id='header-name'>{{ <img src='../../img/americanflag.png' id='flag-img' /> }}</span></a>
+      <a v-for="img in frenchFlag" v-bind:href='img.url'><span id='header-name'>{{ <img src='../../img/frenchflag.jpg' id='flag-img' /> }}</span></a>
+      <a v-for="img in polishFlag" v-bind:href='img.url'><span id='header-name'>{{ <img src='../../img/polishflag.jpg' id='flag-img' /> }}</span></a>
       <span id='header-description'>
         <a v-for="index in indexPage" v-bind:href='index.url'>{{ index.label }} </a>
       </span>
