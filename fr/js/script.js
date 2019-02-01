40743c71ea832390e534a00c21383c52980b48ae
David-Senerchia <45632454+David-Senerchia@users.noreply.github.com>
2019-02-01 09:23:59 -0500

    Update script.js

--- fr/js/script.js
+++ fr/js/script.js
@@ -70,7 +70,7 @@ Vue.component('page-list', {
     var data = {
       pages: [
         { label: 'Accueil', url: indexPrepend + 'fr/index.html' },
-        { label: 'À Propos de Moi', url: pagePrepend + 'about.html' },
+        { label: 'À Propos Moi', url: pagePrepend + 'about.html' },
         { label: 'Développement', url: pagePrepend + 'development.html' },
         { label: 'Écrits', url: pagePrepend + 'writing.html' },
         { label: 'CV', url: pagePrepend + 'CV.html' },
