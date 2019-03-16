0c427ab4d69688799372ed6595ce3cf72c64c985
David-Senerchia <45632454+David-Senerchia@users.noreply.github.com>
2019-03-16 11:20:29 +0000

    Update script.js

--- en/js/script.js
+++ en/js/script.js
@@ -177,7 +177,7 @@ new Vue({
     albums: [
       { title: 'Kiev', img: '../../img/kiev/kiev1.jpeg', album: 'kiev.html', description: 'Kiev, Ukraine' },
       { title: 'Warsaw', img: '../../img/warszawa/warsaw1.jpg', album: 'warszawa.html', description: 'Warsaw, Poland' },
-      { title: 'Warsaw', img: '../../img/warszawa/warsaw1.jpg', album: 'warszawa.html', description: 'Warsaw, Poland' },
+      { title: 'Baltics', img: '../../img/baltic/baltic1.jpg', album: 'baltic.html', description: 'Lithuania, Latvia, Estonia' },
     ]
   }
 });
